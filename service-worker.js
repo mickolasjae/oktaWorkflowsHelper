ced05fbd6e5184bbc49de5e095119e17cb104009
mickolasjae <mick.jae.johnson+github@gmail.com>
2024-02-21 16:27:19 -0800

    everything still works and I just got ports to work from content to service-worker

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+chrome.runtime.onConnect.addListener(function(port) {
+    console.assert(port.name === "flowgram");
+    port.onMessage.addListener(function(msg) {
+    console.log(msg)
+    
+    
+    });
+  });
